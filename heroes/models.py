fdfdc25db4ee1ff71b68d7d133c84459d9133c4a
Oravlah <oravla.hhh@gmail.com>
2025-02-21 19:16:40 -0300

    Se realizaron cambios dentro del modelo de heroes

--- heroes/models.py
+++ heroes/models.py
@@ -4,12 +4,9 @@ from django.db import models
 
 class Heroes(models.Model):
     nombre = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='heroes', blank=True, null=True)
     alias = models.CharField(max_length=100)
     edad = models.IntegerField()
     poder = models.CharField(max_length=100)
     nacionalidad = models.CharField(max_length=100)
     descripcion = models.TextField()
-
-
-    '''def __str__(self):
-        return self.nombre'''
--- projectprinc/settings.py
+++ projectprinc/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
     'users',
     'heroes',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'projectprinc.urls'
 

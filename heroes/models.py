ea6dc2b1228284b2cac29ed11fd4a2625690ef3e
Oravlah <oravla.hhh@gmail.com>
2025-02-28 19:43:27 -0300

    Se realizaron cambios en el modelo de la app Heroes

--- heroes/models.py
+++ heroes/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+import uuid
 
 
 
 class Heroes(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nombre = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='heroes', blank=True, null=True)
     alias = models.CharField(max_length=100)
     edad = models.IntegerField()
     poder = models.CharField(max_length=100)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
